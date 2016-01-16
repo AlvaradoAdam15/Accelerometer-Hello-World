0480b5a21c6489195c11d1b1a5959cd3919bd05f
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-01-16 15:07:55 +0100

    Displaying accelerometer values in Text View

--- app/src/main/java/adam/dam2/iesebre/com/accelerometerhelloworld/MainActivity.java
+++ app/src/main/java/adam/dam2/iesebre/com/accelerometerhelloworld/MainActivity.java
@@ -1,13 +1,59 @@
 package adam.dam2.iesebre.com.accelerometerhelloworld;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+        else {
+            Toast.makeText(this, "Your device don't have accelerometer sensor", Toast.LENGTH_LONG).show();
+        }
+
+        tv = (TextView)findViewById(R.id.accelerometer);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        tv.setText(
+                "X-axis: "+event.values[0]+
+                "\nY-axis: "+event.values[1]+
+                "\nZ-axis: "+event.values[2]);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
